cdd133dba586bfee66374aa2f020bf46fd6186b5
JEONSEUNGREE <jws08219@naver.com>
2021-05-30 23:14:02 +0900

    1_String_BackToFront

--- JAVA/src/BackToFront.java
+++ JAVA/src/BackToFront.java
@@ -3,6 +3,7 @@ public class BackToFront {
         String str = "Information!";
         int n = str.length();
         char st[] = new char[n];
+
         n--;//배열길이 조정
         for (int i = n; i >= 0; i--) {
             st[n-i] = str.charAt(i);
@@ -10,5 +11,6 @@ public class BackToFront {
         for (char i : st) {
             System.out.printf("%c", i);
         }
+
     }
 }
